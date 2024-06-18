c39674e1b0561fe0c45e0b89a04c72b6732158c1
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-06-18 02:02:01 -0400

    refactor: Generalize implementation for initial testing; currently crashes with segfault

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -1,6 +1,7 @@
 #include "tokenizer.h"
 
 #include <cstring>
+#include <filesystem>
 #include <fstream>
 #include <getopt.h>
 
@@ -147,50 +148,41 @@ void free_tokenizer(struct Tokenizer* data) {
 
 int main(int argc, char* argv[]) {
     if (1 == argc) {
-        puts("Usage: vocab [-f <file>] [-v <vocab-type>]");
+        puts("Usage: vocab [-p <path>]");
         return 1;
     }
 
-    const char* const   short_options = "f:v:";
+    const char* const   short_options = "p:";
     const struct option long_options[]
-        = {{"registry-file", required_argument, nullptr, 'f'},
-           {"vocab-type", optional_argument, nullptr, 'v'}};
+        = {{"tokenizer-path", required_argument, nullptr, 'p'}, NULL};
 
     int  opt;
-    char registry_file_path[1024];
-    char vocab_name[5] = "BPE"; // NOTE: avoid name conflict
+    char tokenizer_path[1024];
 
     while ((opt = getopt_long(argc, argv, short_options, long_options, nullptr)) != -1) {
         switch (opt) {
-            case 'f':
+            case 'p':
                 if (optarg == nullptr || strlen(optarg) < 1) {
                     puts("Error: Invalid file path specified.");
                     return 1;
                 }
 
-                strcpy(registry_file_path, optarg);
-                break;
-
-            case 'v':
-                if (optarg == nullptr || strlen(optarg) > 3) {
-                    puts("Error: Invalid vocab type specified.");
-                    return 1;
-                }
-
-                strncpy(vocab_name, optarg, 5);
+                strcpy(tokenizer_path, optarg);
                 break;
 
             default:
-                puts("Usage: vocab [-f <file>] [-v <vocab-type>]");
+                puts("Usage: vocab [-p <tokenizer-path>]");
                 return 1;
         }
     }
 
-    std::ifstream  f(registry_file_path);
+    std::filesystem::path directory = std::filesystem::path(tokenizer_path);
+
+    std::ifstream  f(directory / "tokenizer.json");
     nlohmann::json data = nlohmann::json::parse(f);
 
     if (data.is_null()) {
-        puts("Error: Unable to parse registry file.");
+        puts("Error: Unable to parse tokenizer.json file.");
         return 1;
     }
 
@@ -198,45 +190,11 @@ int main(int argc, char* argv[]) {
 
     fprintf(stdout, "version: %s\n", version.c_str());
 
-    const nlohmann::json model_arch = data["truncation"];
-    const nlohmann::json padding    = data["padding"];
+    struct Tokenizer* tokenizer = malloc_tokenizer(data);
 
-    // for (const auto &config : data) {
-    //     std::cout << "Model Repository:\t" << model_repo << '\n';
-    //     std::cout << "Model Architecture:\t" << model_arch << '\n';
+    fprintf(stdout, "The tokenizer model type is: %s", tokenizer->type().c_str());
 
-    //     // normalizer and pre_tokenizer are either null or an object
-    //     const nlohmann::json normalizer    = config["normalizer"];
-    //     const nlohmann::json pre_tokenizer = config["pre_tokenizer"];
-
-    //     // NOTE: Normalizer may be one of null, Sequence, NFC, NFD, NFKC, NFKD...
-    //     // Seems to be null, Sequence, or NFC in most cases
-    //     // Attempt to handle cases where a key may or may not be present and have a null value
-    //     // Not sure whether to default to NFD or NFC if normalizer is null
-    //     // NOTE: The normalizer type key is not guaranteed
-    //     if (!normalizer.is_null()) {
-    //         const std::string norm_type = normalizer["type"];
-    //         std::cout << "Normalizer Type:\t" << norm_type << std::endl;
-    //         // sequence is an array
-    //         if (0 == norm_type.compare("Sequence")) {
-    //             const nlohmann::json normalizers = normalizer["normalizers"];
-    //             if (normalizers.is_array()) {
-    //                 for (const auto &norm : normalizers.items()) {
-    //                     std::cout << "Norm Sequence Object:\t" << norm.key() << ":\t"
-    //                               << norm.value() << std::endl;
-    //                 }
-    //             }
-    //         } else {
-    //             // otherwise norm_type is an object potentially containing properties
-    //             // this varies from model to model
-    //             // maybe we can just dump the entire object? i don't know yet...
-    //             for (const auto &norm : normalizer.items()) {
-    //                 std::cout << "Pre Object Pair:\t" << norm.key() << ":\t" << norm.value()
-    //                           << std::endl;
-    //             }
-    //         }
-    //     }
-    // }
+    free_tokenizer(tokenizer);
 
     return 0;
 }
